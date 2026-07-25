e2493117b8c720c563642d2e4c92a3e3067e7a03
thanminhhy123 <thanminhhy@gmail.com>
2026-07-25 10:52:10 +0700

    Comment model and migration

---
+++ app/Models/Comment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'blog_id',
+        'user_id',
+        'parent_id',
+        'comment'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function blog()
+    {
+        return $this->belongsTo(Blog::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+}
