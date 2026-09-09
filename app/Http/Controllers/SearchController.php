6805c5c8f305036c0b030cdf6d17dcd491c275f9
thanminhhy123 <thanminhhy@gmail.com>
2026-09-09 14:53:21 +0700

    feat(search): add live search suggestions

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class SearchController extends Controller
 {
     //
-    public function search() {}
+    public function search(Request $request)
+    {
+        //1. Validate data
+        $request->validate([
+            'keyword' => 'nullable|string|max:100'
+        ]);
+
+        //2. Làm sạch 2 đầu của chuỗi
+        $keyword = trim($request->keyword);
+
+        //3. Check whether the input is empty or not
+        if (empty($keyword)) {
+            return response()->json(['html' => '']);
+        }
+
+        //4. Thêm dấu \ trước các ký tự % và _ để thoát ký tự đặc biệt của LIKE(% và _)
+        $safeKeyword = addcslashes($keyword, '%_');
+
+        $products = Product::where('name', 'LIKE', "%{$safeKeyword}%")
+            ->select('id', 'name')
+            ->limit(5)
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'products' => $products
+        ]);
+    }
 }
--- public/frontend/js/search.js
+++ public/frontend/js/search.js
@@ -15,8 +15,31 @@ $(document).ready(function () {
             $.ajax({
                 url: "/api/live-search",
                 type: "GET",
+                // tự động chuyển response từ server ở dạng json sang array/object
                 data: { keyword: keyword },
-                success: function (response) {},
+                success: function (response) {
+                    let products = response.products;
+                    let html = "";
+
+                    if (products.length > 0) {
+                        html += "<ul>";
+                        products.forEach(function (product) {
+                            html += `
+                            <li>
+                                <a href="javascript:void(0)" class="suggest-item">${product.name}</a>
+                            </li>`;
+                        });
+                        html += "</ul>";
+
+                        $("#search-result-box").html(html).show();
+                    } else {
+                        $("#search-result-box")
+                            .html(
+                                "<div class='no-result'>Không tìm thấy kết quả!</div>",
+                            )
+                            .show();
+                    }
+                },
             });
         }, 300);
     });
